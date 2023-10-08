14af60d66aae343bb151157347232f1ee965cd28
debora berte <arobedbee@gmail.com>
2023-10-08 17:54:06 -0300

    First commit with Nix + Docker

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Pokedex project with pure Elixir + ClickHouse database";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = {
+          # ... (previous configuration)
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.elixir pkgs.erlang pkgs.clickhouse ];
+        };
+
+        nixosConfigurations.pokedex = pkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            {
+              services.clickhouse.enable = true;
+
+              environment.systemPackages = with pkgs; [
+                elixir erlang clickhouse
+              ];
+            }
+          ];
+        };
+      }
+    );
+}
